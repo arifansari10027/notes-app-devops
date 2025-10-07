2d5b503e4a4f94ef94ff0c670ac6b09e32641834
Arif Ansari <arifansari10027@gmail.com>
2025-10-07 12:13:56 +0530

    App code commited

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type Note struct {
+	ID      int
+	Content string
+}
+
+var (
+	notes []Note
+	id    int
+	mu    sync.Mutex
+)
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/add", handleAdd)
+	http.HandleFunc("/delete", handleDelete)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	mu.Lock()
+	defer mu.Unlock()
+	tmpl.Execute(w, notes)
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		content := r.FormValue("content")
+		mu.Lock()
+		id++
+		notes = append(notes, Note{ID: id, Content: content})
+		mu.Unlock()
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		idStr := r.FormValue("id")
+		noteID, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+
+		mu.Lock()
+		for i, note := range notes {
+			if note.ID == noteID {
+				notes = append(notes[:i], notes[i+1:]...)
+				break
+			}
+		}
+		mu.Unlock()
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
